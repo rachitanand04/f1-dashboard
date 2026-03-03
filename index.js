b4d6a8e35b7d8f58510ddd8f6db8d42aea526f4f
rachitanand04 <rachit.anand04111@outlook.com>
2026-03-04 01:26:24 +0530

    information box

--- index.js
+++ index.js
@@ -10,26 +10,28 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
 app.post("/", async (req, res) => {
   try {
     const sessionResponse = await axios.get(
-      `${API_URL}/sessions?session_key=${req.body.session}`
+      `${API_URL}/sessions?session_key=${req.body.session}`,
     );
     const sessionData = sessionResponse.data;
 
     const meetingResponse = await axios.get(
-      `${API_URL}/meetings?circuit_key=${sessionData[0].circuit_key}&year=${req.body.year}`
+      `${API_URL}/meetings?circuit_key=${sessionData[0].circuit_key}&year=${req.body.year}`,
     );
     const meetingData = meetingResponse.data;
 
-    const imageURL = meetingData[0].circuit_image;
+    // const imageURL = meetingData[0].circuit_image;
     // console.log(imageURL);
 
-    res.render("index.ejs", { image: imageURL });
+    res.render("index.ejs", {
+      meetingInfo: meetingData[0],
+    });
   } catch (error) {
     console.log(error.response?.data || error.message);
   }
@@ -38,4 +40,3 @@ app.post("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
